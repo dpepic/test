1e95bdb806291d8fd635059f1e5b282fbbb661d3
Dragan Pepic <d.pepic@hotmail.com>
2019-10-19 18:54:49 +0200

    Sta je ta izmena

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -12,15 +12,7 @@ namespace ConsoleApp3
         {
             int[] pocetniNiz = { 3, 4, 5, 7, 10, 20 };
 
-            Console.WriteLine("Za koliko mesta menjate niz?");
-            int broj = int.Parse(Console.ReadLine());
-
-            int[] noviNiz = new int[pocetniNiz.Length + broj];
-
-            for (int indeks = 0; indeks < noviNiz.Length; indeks++)
-            {
-                noviNiz[indeks] = pocetniNiz[indeks];
-            }
+        
 
             pocetniNiz = noviNiz;
 
