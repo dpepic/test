15e2fabfc9c031b59bc8bd56928f05bc7efbb5e3
Dragan Pepic <d.pepic@hotmail.com>
2019-10-19 18:51:27 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pocetniNiz = { 3, 4, 5, 7, 10, 20 };
+
+            Console.WriteLine("Za koliko mesta menjate niz?");
+            int broj = int.Parse(Console.ReadLine());
+
+            int[] noviNiz = new int[pocetniNiz.Length + broj];
+
+            for (int indeks = 0; indeks < noviNiz.Length; indeks++)
+            {
+                noviNiz[indeks] = pocetniNiz[indeks];
+            }
+
+            pocetniNiz = noviNiz;
+
+            Console.ReadKey();
+        }
+    }
+}
